4bbae10e5751d25f1cd8234c33865058795f0d98
susannasouv <ssouv@berkeley.edu>
2016-03-06 02:13:33 -0800

    StartScreenView and GameView completed. Correct button function implemented. Image changes added to a guessButtonTap function that will be needed for Part 2.

--- Hangman/Hangman/GameViewController.swift
+++ Hangman/Hangman/GameViewController.swift
@@ -10,13 +10,43 @@ import UIKit
 
 class GameViewController: UIViewController {
 
+    // MARK: Properties
+    @IBOutlet weak var hangmanStateImage: UIImageView!
+    @IBOutlet weak var hangmanPhraseLabel: UILabel!
+    @IBOutlet weak var phraseGuessTextField: UITextField!
+    @IBOutlet weak var correctButton: UIButton!
+    @IBOutlet weak var incorrectGuessesLabel: UILabel!
+    
+    var numIncorrect = 0
+
+    let placeHolderChar: Character = "*"
+    var hangmanPhraseString: String = ""
+    var incorrectGuessesArray = [Character]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         let hangmanPhrases = HangmanPhrases()
-        var phrase = hangmanPhrases.getRandomPhrase()
+        let phrase = hangmanPhrases.getRandomPhrase()
+        hangmanPhraseString = phrase
+        
+        var hangmanPhraseText = ""
+        for index in phrase.characters.indices {
+            if phrase[index] == " " {
+                hangmanPhraseText.append(Character(" "))
+            }
+            else {
+                hangmanPhraseText.append(placeHolderChar)
+            }
+        }
+        hangmanPhraseLabel.text = hangmanPhraseText
         print(phrase)
+        correctButton.addTarget(self, action: "guessButtonTap", forControlEvents: .TouchUpInside)
+        hangmanStateImage.image = UIImage(named: "hangman1.gif")
+        
+        
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,7 +54,84 @@ class GameViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func isWinState() -> Bool {
+        return !hangmanPhraseLabel.text!.characters.contains(placeHolderChar)
+    }
+    
+    func isLoseState() -> Bool {
+        return numIncorrect >= 6
+    }
 
+    func setHangmanStateImage() {
+        // going to assume that numIncorrect < 7
+        var imageName = "hangman"
+        imageName.insertContentsOf(String(numIncorrect + 1).characters, at: imageName.endIndex)
+        imageName.insertContentsOf(".gif".characters, at: imageName.endIndex)
+        hangmanStateImage.image = UIImage(named: imageName)
+    }
+    
+    func correctButtonTap () {
+        // removes the first placeHolderChar in hangmanPhraseLabel.text
+        if let phraseSoFar = hangmanPhraseLabel.text {
+            for index in phraseSoFar.characters.indices {
+                if phraseSoFar[index] == placeHolderChar {
+                    hangmanPhraseLabel.text!.removeAtIndex(index)
+                    hangmanPhraseLabel.text!.insert(hangmanPhraseString[index], atIndex: index)
+//                    hangmanPhraseLabel.text!.removeAtIndex(index.successor())
+// above line, combined with the line above it does not work for some reason
+                    return
+                }
+            }
+        }
+
+        
+    }
+    
+    func incorrectButtonTap () {
+        
+    }
+    
+    func guessButtonTap() {
+        if let phraseGuessTextFieldContent = phraseGuessTextField.text {
+            if phraseGuessTextFieldContent.isEmpty {
+                return
+            }
+        }
+        let guessChar: Character = (phraseGuessTextField.text?.characters[(phraseGuessTextField.text?.characters.startIndex)!])!
+        var tempString = hangmanPhraseLabel.text
+        if hangmanPhraseString.characters.contains(guessChar) {
+            for index in (tempString!.characters.indices) {
+                if hangmanPhraseString.characters[index] == guessChar && tempString?.characters[index] == placeHolderChar {
+                    tempString?.removeAtIndex(index)
+                    tempString?.insert(guessChar, atIndex: index)
+                }
+            }
+            hangmanPhraseLabel.text = tempString
+        }
+        else {
+            updateIncorrectGuesses(guessChar)
+        }
+        phraseGuessTextField.text = ""
+    }
+    
+    func updateIncorrectGuesses(guessChar : Character) {
+        if incorrectGuessesArray.contains(guessChar) {
+            return
+        }
+        else {
+            numIncorrect += 1
+            incorrectGuessesArray.append(guessChar)
+            incorrectGuessesLabel.text = "Incorrect guesses: \n"
+            for incorrectGuess in incorrectGuessesArray {
+                incorrectGuessesLabel.text?.append(incorrectGuess)
+                incorrectGuessesLabel.text?.append(Character(" "))
+            }
+            if !isLoseState() {
+                setHangmanStateImage()
+            }
+            
+        }
+    }
     /*
     // MARK: - Navigation
 
